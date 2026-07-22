7817ed4db163bebeacdf9fcba126ab5cd267c931
Allenchu-aws <allen@jvd.tw>
2026-07-22 14:36:17 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // ⚠️ 把下面兩個值換成你自己 Supabase 專案的資訊
 // 到 supabase.com 建立專案後，在 Project Settings → API 頁面可以找到
-const SUPABASE_URL = 'https://xrzxfkhmsjcqdwitxsgg.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://xrzxfkhmsjcqdwitxsgg.supabase.co/rest/v1';
 const SUPABASE_ANON_KEY = 'sb_publishable_vNDtfPXsGxOoWbWyL9Fgdw_h_SMqrjh';
 
 // anon public key 設計上就是給前端公開使用的，不是機密資訊，
